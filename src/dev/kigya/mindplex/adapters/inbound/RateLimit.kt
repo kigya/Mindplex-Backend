20b387c26c968fa388a3588a79f3007ef35f21e8
kigya <kirillborichevskiy@gmail.com>
2025-03-27 16:22:16 +0300

    MPB-3: Implemented rate limiting by key

--- src/dev/kigya/mindplex/adapters/inbound/RateLimit.kt
+++ src/dev/kigya/mindplex/adapters/inbound/RateLimit.kt
@@ -1,12 +1,16 @@
 package dev.kigya.mindplex.adapters.inbound
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.ratelimit.*
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRateLimit() {
     install(RateLimit) {
         register(RateLimitName(RateLimitKey.QUESTIONS)) {
+            requestKey { call ->
+                call.request.origin.remoteHost
+            }
             rateLimiter(
                 limit = 1,
                 refillPeriod = 5.seconds,
